403dd5e16b61c9e87b52a8a43789608dc001ff7a
willgbryan <will.bryan421@gmail.com>
2024-04-10 15:02:48 -0500

    check for uploads directory first

--- reach_core/utils/unstructured_functions.py
+++ reach_core/utils/unstructured_functions.py
@@ -12,13 +12,16 @@ def run_command(command: str) -> str:
 async def process_unstructured(upload_dir: str = "uploads") -> list:
 
     output_list = []
-    for filename in os.listdir(upload_dir):
-        file_path = os.path.join(upload_dir, filename)
-        if os.path.isfile(file_path):
-            # TODO plenty of room to investigate different extraction methods, times, and strategies
-            elements = partition(filename=file_path, strategy='fast')
-            raw_content = "\n\n".join([str(el) for el in elements])
-            output_list.append({'url': file_path, 'raw_content': raw_content})
+    if os.path.exists(upload_dir):
+        for filename in os.listdir(upload_dir):
+            file_path = os.path.join(upload_dir, filename)
+            if os.path.isfile(file_path):
+                # TODO plenty of room to investigate different extraction methods, times, and strategies
+                elements = partition(filename=file_path, strategy='fast')
+                raw_content = "\n\n".join([str(el) for el in elements])
+                output_list.append({'url': file_path, 'raw_content': raw_content})
+    else:
+        print(f"The directory {upload_dir} does not exist.")
 
     return output_list
 
