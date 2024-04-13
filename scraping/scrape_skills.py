d0995630c295b8494bd38035dd383c5bf695c923
willgbryan <will.bryan421@gmail.com>
2024-04-12 20:55:51 -0500

    youtube transcript extract function

--- scraping/scrape_skills.py
+++ scraping/scrape_skills.py
@@ -1,5 +1,7 @@
 from langchain.document_loaders import PyMuPDFLoader
 from langchain.retrievers import ArxivRetriever
+from youtube_transcript_api import YouTubeTranscriptApi
+
 
 
 def scrape_pdf_with_pymupdf(url) -> str:
@@ -29,3 +31,23 @@ def scrape_pdf_with_arxiv(query) -> str:
     retriever = ArxivRetriever(load_max_docs=2, doc_content_chars_max=None)
     docs = retriever.get_relevant_documents(query=query)
     return docs[0].page_content
+
+def scrape_youtube_transcripts(url: str) -> str:
+    """Scrape transcript from a Youtube video url
+    
+    Args:
+        url (str): The video url to scrape
+        
+    Returns:
+        str: The transcript from the video
+    """
+
+    video_id = url.replace('https://www.youtube.com/watch?v=', '')
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    output=''
+
+    for x in transcript:
+        sentence = x['text']
+        output += f' {sentence}\n'
+
+    return output
