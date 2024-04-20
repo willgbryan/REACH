3029d98dfcefd245b80ed89bdc3988837e40311c
willgbryan <will.bryan421@gmail.com>
2024-04-20 17:39:58 -0500

    only call unstructured if not audio filetype

--- reach_core/utils/unstructured_functions.py
+++ reach_core/utils/unstructured_functions.py
@@ -16,7 +16,7 @@ async def process_unstructured(upload_dir: str = "uploads") -> List[Dict[str, st
     if os.path.exists(upload_dir):
         for filename in os.listdir(upload_dir):
             file_path = os.path.join(upload_dir, filename)
-            if os.path.isfile(file_path):
+            if not (os.path.isfile(file_path) and not file_path.endswith(('.mp3', '.wav', '.flac'))):
                 # TODO plenty of room to investigate different extraction methods, times, and strategies
                 # TODO list element extraction strategies
                 elements = partition(filename=file_path, strategy='fast')
