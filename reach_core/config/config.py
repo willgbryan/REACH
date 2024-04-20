fc0954086cb146c263f1db28dd3f336692451c5b
willgbryan <will.bryan421@gmail.com>
2024-04-20 14:35:25 -0500

    Update config.py

--- reach_core/config/config.py
+++ reach_core/config/config.py
@@ -8,25 +8,34 @@ class Config:
 
     def __init__(self, config_file: str = None):
         """Initialize the config class."""
-        self.config_file = config_file if config_file else os.getenv('CONFIG_FILE')
-        self.retriever = os.getenv('SEARCH_RETRIEVER', "searx")
-        self.embedding_provider = os.getenv('EMBEDDING_PROVIDER', 'openai')
-        self.llm_provider = os.getenv('LLM_PROVIDER', "openai")
-        self.fast_llm_model = os.getenv('FAST_LLM_MODEL', "gpt-3.5-turbo-16k") #gpt-3.5-turbo-16k
-        self.smart_llm_model = os.getenv('SMART_LLM_MODEL', "gpt-4-1106-preview") #gpt-4-1106-preview
-        self.fast_token_limit = int(os.getenv('FAST_TOKEN_LIMIT', 2000))
-        self.smart_token_limit = int(os.getenv('SMART_TOKEN_LIMIT', 4000))
-        self.browse_chunk_max_length = int(os.getenv('BROWSE_CHUNK_MAX_LENGTH', 8192))
-        self.summary_token_limit = int(os.getenv('SUMMARY_TOKEN_LIMIT', 700))
-        self.temperature = float(os.getenv('TEMPERATURE', 0.55))
-        self.user_agent = os.getenv('USER_AGENT', "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                                                   "(KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0")
-        self.max_search_results_per_query = int(os.getenv('MAX_SEARCH_RESULTS_PER_QUERY', 5))
-        self.memory_backend = os.getenv('MEMORY_BACKEND', "local")
-        self.total_words = int(os.getenv('TOTAL_WORDS', 10))
-        self.report_format = os.getenv('REPORT_FORMAT', "csv")
-        self.max_iterations = int(os.getenv('MAX_ITERATIONS', 8))
-        self.agent_role = os.getenv('AGENT_ROLE', None)
+        self.config_file = config_file if config_file else os.getenv("CONFIG_FILE")
+        self.retriever = os.getenv("SEARCH_RETRIEVER", "searx")
+        self.embedding_provider = os.getenv("EMBEDDING_PROVIDER", "openai")
+        self.llm_provider = os.getenv("LLM_PROVIDER", "openai")
+        self.fast_llm_model = os.getenv(
+            "FAST_LLM_MODEL", "gpt-3.5-turbo-16k"
+        )  # mistral:instruct
+        self.smart_llm_model = os.getenv(
+            "SMART_LLM_MODEL", "gpt-4-1106-preview"
+        )  # mistral:instruct
+        self.fast_token_limit = int(os.getenv("FAST_TOKEN_LIMIT", 2000))
+        self.smart_token_limit = int(os.getenv("SMART_TOKEN_LIMIT", 4000))
+        self.browse_chunk_max_length = int(os.getenv("BROWSE_CHUNK_MAX_LENGTH", 8192))
+        self.summary_token_limit = int(os.getenv("SUMMARY_TOKEN_LIMIT", 700))
+        self.temperature = float(os.getenv("TEMPERATURE", 0.55))
+        self.user_agent = os.getenv(
+            "USER_AGENT",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+            "(KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
+        )
+        self.max_search_results_per_query = int(
+            os.getenv("MAX_SEARCH_RESULTS_PER_QUERY", 5)
+        )
+        self.memory_backend = os.getenv("MEMORY_BACKEND", "local")
+        self.total_words = int(os.getenv("TOTAL_WORDS", 10))
+        self.report_format = os.getenv("REPORT_FORMAT", "csv")
+        self.max_iterations = int(os.getenv("MAX_ITERATIONS", 5))
+        self.agent_role = os.getenv("AGENT_ROLE", None)
         self.scraper = os.getenv("SCRAPER", "bs")
         self.max_subtopics = os.getenv("MAX_SUBTOPICS", 8)
 
@@ -40,4 +49,3 @@ class Config:
             config = json.load(f)
         for key, value in config.items():
             self.__dict__[key] = value
-
