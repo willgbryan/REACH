a0b458bacb3ff25742d27bb35994e959f519a546
willgbryan <will.bryan421@gmail.com>
2024-05-17 13:46:22 -0500

    llama cpp updates

---
+++ llama-cpp/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 2914;
+char const *LLAMA_COMMIT = "82ca83db";
+char const *LLAMA_COMPILER = "Apple clang version 15.0.0 (clang-1500.1.0.2.5)";
+char const *LLAMA_BUILD_TARGET = "arm64-apple-darwin23.3.0";
