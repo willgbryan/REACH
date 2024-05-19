6630cc014fd713b9780dec1b814afc47ac0f5953
willgbryan <will.bryan421@gmail.com>
2024-05-19 18:04:10 -0500

    working settings button

--- reach-next-app/src/pages/index.tsx
+++ reach-next-app/src/pages/index.tsx
@@ -1,6 +1,16 @@
+"use client"
 import { Button } from "@/components/ui/button"
 import { TooltipTrigger, TooltipContent, Tooltip, TooltipProvider } from "@/components/ui/tooltip"
-import { DrawerTrigger, DrawerTitle, DrawerDescription, DrawerHeader, DrawerContent, Drawer } from "@/components/ui/drawer"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
 import { Label } from "@/components/ui/label"
 import { SelectValue, SelectTrigger, SelectItem, SelectContent, Select } from "@/components/ui/select"
 import { Input } from "@/components/ui/input"
@@ -28,7 +38,7 @@ export function MainPage() {
                 </Button>
               </TooltipTrigger>
               <TooltipContent side="right" sideOffset={5}>
-                Playground
+                Canvas
               </TooltipContent>
             </Tooltip>
             <Tooltip>
@@ -63,9 +73,114 @@ export function MainPage() {
             </Tooltip>
             <Tooltip>
               <TooltipTrigger asChild>
-                <Button aria-label="Settings" className="rounded-lg" size="icon" variant="ghost">
-                  <Settings2Icon className="size-5" />
-                </Button>
+                <Drawer>
+                <DrawerTrigger asChild>
+                  <Button aria-label="Settings" className="rounded-lg" size="icon" variant="ghost">
+                    <Settings2Icon className="size-5" />
+                  </Button>
+                </DrawerTrigger>
+                <DrawerContent className="max-h-[80vh]">
+                  <DrawerHeader>
+                    <DrawerTitle>Configuration</DrawerTitle>
+                    <DrawerDescription>Configure the settings for the model and messages.</DrawerDescription>
+                  </DrawerHeader>
+                  <form className="grid w-full items-start gap-6 overflow-auto p-4 pt-0">
+                    <fieldset className="grid gap-6 rounded-lg border p-4">
+                      <legend className="-ml-1 px-1 text-sm font-medium">Settings</legend>
+                      <div className="grid gap-3">
+                        <Label htmlFor="model">Model</Label>
+                        <Select>
+                          <SelectTrigger className="items-start [&_[data-description]]:hidden" id="model">
+                            <SelectValue placeholder="Select a model" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="genesis">
+                              <div className="flex items-start gap-3 text-muted-foreground">
+                                <RabbitIcon className="size-5" />
+                                <div className="grid gap-0.5">
+                                  <p>
+                                    Neural
+                                    <span className="font-medium text-foreground">Genesis</span>
+                                  </p>
+                                  <p className="text-xs" data-description>
+                                    Our fastest model for general use cases.
+                                  </p>
+                                </div>
+                              </div>
+                            </SelectItem>
+                            <SelectItem value="explorer">
+                              <div className="flex items-start gap-3 text-muted-foreground">
+                                <BirdIcon className="size-5" />
+                                <div className="grid gap-0.5">
+                                  <p>
+                                    Neural
+                                    <span className="font-medium text-foreground">Explorer</span>
+                                  </p>
+                                  <p className="text-xs" data-description>
+                                    Performance and speed for efficiency.
+                                  </p>
+                                </div>
+                              </div>
+                            </SelectItem>
+                            <SelectItem value="quantum">
+                              <div className="flex items-start gap-3 text-muted-foreground">
+                                <TurtleIcon className="size-5" />
+                                <div className="grid gap-0.5">
+                                  <p>
+                                    Neural
+                                    <span className="font-medium text-foreground">Quantum</span>
+                                  </p>
+                                  <p className="text-xs" data-description>
+                                    The most powerful model for complex computations.
+                                  </p>
+                                </div>
+                              </div>
+                            </SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </div>
+                      <div className="grid gap-3">
+                        <Label htmlFor="temperature">Temperature</Label>
+                        <Input id="temperature" placeholder="0.4" type="number" />
+                      </div>
+                      <div className="grid gap-3">
+                        <Label htmlFor="top-p">Top P</Label>
+                        <Input id="top-p" placeholder="0.7" type="number" />
+                      </div>
+                      <div className="grid gap-3">
+                        <Label htmlFor="top-k">Top K</Label>
+                        <Input id="top-k" placeholder="0.0" type="number" />
+                      </div>
+                    </fieldset>
+                    <fieldset className="grid gap-6 rounded-lg border p-4">
+                      <legend className="-ml-1 px-1 text-sm font-medium">Messages</legend>
+                      <div className="grid gap-3">
+                        <Label htmlFor="role">Role</Label>
+                        <Select defaultValue="system">
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select a role" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="system">System</SelectItem>
+                            <SelectItem value="user">User</SelectItem>
+                            <SelectItem value="assistant">Assistant</SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </div>
+                      <div className="grid gap-3">
+                        <Label htmlFor="content">Content</Label>
+                        <Textarea id="content" placeholder="You are a..." />
+                      </div>
+                    </fieldset>
+                    <DrawerFooter>
+                      <Button>Submit</Button>
+                      <DrawerClose>
+                        <Button variant="outline">Cancel</Button>
+                      </DrawerClose>
+                  </DrawerFooter>
+                  </form>
+                </DrawerContent>
+              </Drawer>
               </TooltipTrigger>
               <TooltipContent side="right" sideOffset={5}>
                 Settings
@@ -100,209 +215,13 @@ export function MainPage() {
       </aside>
       <div className="flex flex-col">
         <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b bg-background px-4">
-          <h1 className="text-xl font-semibold">Playground</h1>
-          <Drawer>
-            <DrawerTrigger asChild>
-              <Button className="md:hidden" size="icon" variant="ghost">
-                <SettingsIcon className="size-4" />
-                <span className="sr-only">Settings</span>
-              </Button>
-            </DrawerTrigger>
-            <DrawerContent className="max-h-[80vh]">
-              <DrawerHeader>
-                <DrawerTitle>Configuration</DrawerTitle>
-                <DrawerDescription>Configure the settings for the model and messages.</DrawerDescription>
-              </DrawerHeader>
-              <form className="grid w-full items-start gap-6 overflow-auto p-4 pt-0">
-                <fieldset className="grid gap-6 rounded-lg border p-4">
-                  <legend className="-ml-1 px-1 text-sm font-medium">Settings</legend>
-                  <div className="grid gap-3">
-                    <Label htmlFor="model">Model</Label>
-                    <Select>
-                      <SelectTrigger className="items-start [&_[data-description]]:hidden" id="model">
-                        <SelectValue placeholder="Select a model" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="genesis">
-                          <div className="flex items-start gap-3 text-muted-foreground">
-                            <RabbitIcon className="size-5" />
-                            <div className="grid gap-0.5">
-                              <p>
-                                Neural
-                                <span className="font-medium text-foreground">Genesis</span>
-                              </p>
-                              <p className="text-xs" data-description>
-                                Our fastest model for general use cases.
-                              </p>
-                            </div>
-                          </div>
-                        </SelectItem>
-                        <SelectItem value="explorer">
-                          <div className="flex items-start gap-3 text-muted-foreground">
-                            <BirdIcon className="size-5" />
-                            <div className="grid gap-0.5">
-                              <p>
-                                Neural
-                                <span className="font-medium text-foreground">Explorer</span>
-                              </p>
-                              <p className="text-xs" data-description>
-                                Performance and speed for efficiency.
-                              </p>
-                            </div>
-                          </div>
-                        </SelectItem>
-                        <SelectItem value="quantum">
-                          <div className="flex items-start gap-3 text-muted-foreground">
-                            <TurtleIcon className="size-5" />
-                            <div className="grid gap-0.5">
-                              <p>
-                                Neural
-                                <span className="font-medium text-foreground">Quantum</span>
-                              </p>
-                              <p className="text-xs" data-description>
-                                The most powerful model for complex computations.
-                              </p>
-                            </div>
-                          </div>
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="temperature">Temperature</Label>
-                    <Input id="temperature" placeholder="0.4" type="number" />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="top-p">Top P</Label>
-                    <Input id="top-p" placeholder="0.7" type="number" />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="top-k">Top K</Label>
-                    <Input id="top-k" placeholder="0.0" type="number" />
-                  </div>
-                </fieldset>
-                <fieldset className="grid gap-6 rounded-lg border p-4">
-                  <legend className="-ml-1 px-1 text-sm font-medium">Messages</legend>
-                  <div className="grid gap-3">
-                    <Label htmlFor="role">Role</Label>
-                    <Select defaultValue="system">
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a role" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="system">System</SelectItem>
-                        <SelectItem value="user">User</SelectItem>
-                        <SelectItem value="assistant">Assistant</SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="content">Content</Label>
-                    <Textarea id="content" placeholder="You are a..." />
-                  </div>
-                </fieldset>
-              </form>
-            </DrawerContent>
-          </Drawer>
+          <h1 className="text-xl font-semibold">Reach</h1>
           <Button className="ml-auto gap-1.5 text-sm" size="sm" variant="outline">
             <ShareIcon className="size-3.5" />
             Share
           </Button>
         </header>
-        <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
-          <div className="relative hidden flex-col items-start gap-8 md:flex" x-chunk="dashboard-03-chunk-0">
-            <form className="grid w-full items-start gap-6">
-              <fieldset className="grid gap-6 rounded-lg border p-4">
-                <legend className="-ml-1 px-1 text-sm font-medium">Settings</legend>
-                <div className="grid gap-3">
-                  <Label htmlFor="model">Model</Label>
-                  <Select>
-                    <SelectTrigger className="items-start [&_[data-description]]:hidden" id="model">
-                      <SelectValue placeholder="Select a model" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="genesis">
-                        <div className="flex items-start gap-3 text-muted-foreground">
-                          <RabbitIcon className="size-5" />
-                          <div className="grid gap-0.5">
-                            <p>
-                              Neural
-                              <span className="font-medium text-foreground">Genesis</span>
-                            </p>
-                            <p className="text-xs" data-description>
-                              Our fastest model for general use cases.
-                            </p>
-                          </div>
-                        </div>
-                      </SelectItem>
-                      <SelectItem value="explorer">
-                        <div className="flex items-start gap-3 text-muted-foreground">
-                          <BirdIcon className="size-5" />
-                          <div className="grid gap-0.5">
-                            <p>
-                              Neural
-                              <span className="font-medium text-foreground">Explorer</span>
-                            </p>
-                            <p className="text-xs" data-description>
-                              Performance and speed for efficiency.
-                            </p>
-                          </div>
-                        </div>
-                      </SelectItem>
-                      <SelectItem value="quantum">
-                        <div className="flex items-start gap-3 text-muted-foreground">
-                          <TurtleIcon className="size-5" />
-                          <div className="grid gap-0.5">
-                            <p>
-                              Neural
-                              <span className="font-medium text-foreground">Quantum</span>
-                            </p>
-                            <p className="text-xs" data-description>
-                              The most powerful model for complex computations.
-                            </p>
-                          </div>
-                        </div>
-                      </SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div className="grid gap-3">
-                  <Label htmlFor="temperature">Temperature</Label>
-                  <Input id="temperature" placeholder="0.4" type="number" />
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                  <div className="grid gap-3">
-                    <Label htmlFor="top-p">Top P</Label>
-                    <Input id="top-p" placeholder="0.7" type="number" />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="top-k">Top K</Label>
-                    <Input id="top-k" placeholder="0.0" type="number" />
-                  </div>
-                </div>
-              </fieldset>
-              <fieldset className="grid gap-6 rounded-lg border p-4">
-                <legend className="-ml-1 px-1 text-sm font-medium">Messages</legend>
-                <div className="grid gap-3">
-                  <Label htmlFor="role">Role</Label>
-                  <Select defaultValue="system">
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select a role" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="system">System</SelectItem>
-                      <SelectItem value="user">User</SelectItem>
-                      <SelectItem value="assistant">Assistant</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div className="grid gap-3">
-                  <Label htmlFor="content">Content</Label>
-                  <Textarea className="min-h-[9.5rem]" id="content" placeholder="You are a..." />
-                </div>
-              </fieldset>
-            </form>
-          </div>
+        <main className="grid flex-1 gap-4 overflow-auto p-4">
           <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
             <Badge className="absolute right-3 top-3" variant="outline">
               Output
